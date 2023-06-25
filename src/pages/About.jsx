59d9dfbdb47b6a4eac11053163f5dbca1d8646f8
dekzyd <adekoyadayo2010@gmail.com>
2023-06-25 14:22:57 +0100

    initial commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+import { Wrapper } from "../assets/wrappers/AboutPage";
+
+const About = () => {
+  return (
+    <Wrapper>
+      <h3>About</h3>
+    </Wrapper>
+  );
+};
+
+export default About;
