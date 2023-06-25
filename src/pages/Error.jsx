f2b761b2f28dc8417173a2d00c1466f7e80b2aa4
dekzyd <adekoyadayo2010@gmail.com>
2023-06-25 17:31:54 +0100

    add 404 error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,5 +1,30 @@
+import Wrapper from "../assets/wrappers/ErrorPage";
+import { Link, useRouteError } from "react-router-dom";
+import img from "../assets/not-found.svg";
+
 const Error = () => {
-  return <h2>Error</h2>;
+  const error = useRouteError();
+  console.log(error);
+
+  if (error.status === 404) {
+    return (
+      <Wrapper>
+        <div>
+          <img src={img} alt="not found" />
+          <h3>Oh oh!</h3>
+          <p>we can't find the page you are looking for.</p>
+          <Link to="/">Back Home</Link>
+        </div>
+      </Wrapper>
+    );
+  }
+  return (
+    <Wrapper>
+      <div>
+        <h3>Something went wrong</h3>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default Error;
