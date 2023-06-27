b29e16b7c08f580cae9b0bffaaff4d298c9c0f86
dekzyd <adekoyadayo2010@gmail.com>
2023-06-27 12:57:21 +0100

    add searchForm to homepage

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import Wrapper from "../assets/wrappers/SearchForm";
+import { Form, useNavigation } from "react-router-dom";
 
 const SearchForm = () => {
-  return <h2>SearchForm</h2>;
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === "submitting";
+  return (
+    <Wrapper>
+      <Form className="form">
+        <input
+          type="search"
+          name="search"
+          defaultValue="vodka"
+          className="form-input"
+        />
+        <button type="submit" className="btn" disabled={isSubmitting}>
+          {isSubmitting ? "submitting..." : "search"}
+        </button>
+      </Form>
+    </Wrapper>
+  );
 };
 
 export default SearchForm;
